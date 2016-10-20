bd2d7fbdb8e8ea71535222157968c30932e19598
eunaumtenhoid <eunaumtenhoid@outlook.com>
2016-10-20 20:31:37 -0200

    Update common.php

--- rmcgirr83/elonw/language/pt-br/common.php
+++ rmcgirr83/elonw/language/pt-br/common.php
@@ -34,5 +34,5 @@ if (empty($lang) || !is_array($lang))
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
 $lang = array_merge($lang, array(
-	'ELONW'	=> 'Abre uma nova janelaA',
+	'ELONW'	=> 'Abre uma nova janela',
 ));

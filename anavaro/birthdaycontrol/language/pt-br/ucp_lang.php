33b26dc58220f8049dc971b0a90c7e7951bf7a63
eunaumtenhoid <eunaumtenhoid@outlook.com>
2016-10-20 21:33:10 -0200

    Update ucp_lang.php

--- anavaro/birthdaycontrol/language/pt-br/ucp_lang.php
+++ anavaro/birthdaycontrol/language/pt-br/ucp_lang.php
@@ -23,9 +23,9 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'BIRTH_DATE'	=> 'Data de nascimento',
-	'BDAY_NO_DATE'	=> 'Por favor selecione a data de nascimento. Nao sera possivel registrar-se neste forum se voce nao inserir sua data de nascimento.',
-	'BDAY_TO_YOUNG'	=> 'Voce nao tem a idade minima para registrar-se neste forum. <br /> Este forum exige a idade minima de %1$d anos.',
+	'BDAY_NO_DATE'	=> 'Por favor selecione a data de nascimento. Não será possível registrar-se neste forum se você não inserir sua data de nascimento.',
+	'BDAY_TO_YOUNG'	=> 'Você não tem a idade mínima para registrar-se neste forum. <br /> Este forum exige a idade mínima de %1$d anos.',
 	'BC_SHOW_BDAY'	=> 'Mostrar idade',
-	'BDAY_NA'	=> 'nao aplicavel',
+	'BDAY_NA'	=> 'n/a',
 
 ));

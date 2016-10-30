c62b6a2ed34468f2bf1ea76063d6461b4543debd
eunaumtenhoid <eunaumtenhoid@outlook.com>
2016-10-29 22:21:35 -0200

    Update info_acp_posts_merging.php

--- rxu/PostsMerging/language/pt-br/info_acp_posts_merging.php
+++ rxu/PostsMerging/language/pt-br/info_acp_posts_merging.php
@@ -34,12 +34,15 @@ if (empty($lang) || !is_array($lang))
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
 $lang = array_merge($lang, array(
-	'ACP_POSTS_MERGING'				=> 'Posts merging',
-	'ACP_POSTS_MERGING_EXPLAIN'		=> 'Here you can apply settings for Posts merging extension.',
-	'MERGE_INTERVAL'				=> 'Posts merging interval',
-	'MERGE_INTERVAL_EXPLAIN'		=> 'If a user submit more than 2 posts in this time period, posts will be merged into one post. Information about the time passed from the previous post submitting will be added (for each post). Leave blank or set 0 to disable posts merging.',
-	'MERGE_NO_TOPICS'				=> 'Excluded topics',
-	'MERGE_NO_TOPICS_EXPLAIN'		=> 'Comma separated list of topics ids where posts merging should not be applied to, when the feature is enabled.',
-	'MERGE_NO_FORUMS'				=> 'Excluded forums',
-	'MERGE_NO_FORUMS_EXPLAIN'		=> 'Comma separated list of forums ids where posts merging should not be applied to, when the feature is enabled.',
+	'ACP_POSTS_MERGING'				=> 'Fundir mensagens',
+	'ACP_POSTS_MERGING_EXPLAIN'		=> 'Aqui você pode aplicar as configurações da extensão Posts merging.',
+	'ACP_POSTS_MERGING_SEPARATOR_PREVIEW'	=> 'Prever separador',
+	'MERGE_INTERVAL'				=> 'Intervalo',
+	'MERGE_INTERVAL_EXPLAIN'		=> 'Se um usuário enviar mais que 2 mensagens neste período de tempo, as mensagens serão fundidas numa única mensagem. Informação sobre horário, que será baseado de acordo a com última mensagem enviada, será adicionada para cada mensagem. Deixe em branco ou defina 0 para desativar este recurso.',
+	'MERGE_NO_TOPICS'				=> 'Excluir estes tópicos',
+	'MERGE_NO_TOPICS_EXPLAIN'		=> 'Lista de ID dos tópicos, separado por vírgula, onde o recurso não será aplicado. Essa opção será aplicada somente se o recurso de fundir mensagens estiver ativado.',
+	'MERGE_NO_FORUMS'				=> 'Excluir estes fóruns',
+	'MERGE_NO_FORUMS_EXPLAIN'		=> 'Este recurso <strong>será desativado nos fóruns selecionados</strong>. Selecione nenhum para usar este recurso em todos os fóruns.<br />Selecione ou desmarque múltiplos fóruns pressionando <samp>CTRL</samp> e clicando.',
+	'MERGE_SEPARATOR'				=> 'Separador',
+	'MERGE_SEPARATOR_EXPLAIN'		=> 'Aqui você pode configurar o separador que será exibido entre as mensagens fundidas.<br />Você poderá usar BBCodes de acordo com as configurações do fórum ou configuração de mensagens.<br /><br />Você também pode usar qualquer string de idioma dos arquivos do pacote de idioma: {L_<em>&lt;STRINGNAME&gt;</em>} onde <em>&lt;STRINGNAME&gt;</em> é o nome da string traduzida que você quer adicionar. Por exemplo, {L_WROTE} será exibido como “escreveu”.<br /><br />Use o marcador <em>&#37;s</em> (apenas uma vez) para incluir o horário entre o separador de mensagens fundidas.',
 ));
